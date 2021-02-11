c40d3934206068e582b928e276219002312e71c6
Imari Pickens <imaridp23@gmail.com>
2021-02-10 19:43:07 -0500

    Populating data for burger table

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+-- POPULATING DATA ENTRIES FOR BURGERS TABLE
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Terminator", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Good Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Mondo Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Holy Chicken", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Jerky Turkey", FALSE);
